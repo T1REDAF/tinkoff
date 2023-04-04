c13cd4c1977d130d210d1b175b1771793ee7b884
T1REDAF <dinar5440@bk.ru>
2023-04-04 21:43:40 +0500

    Solved task 2

--- bot/src/main/java/ru/tinkoff/edu/java/bot/bot/TinkoffBot.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/bot/TinkoffBot.java
@@ -3,13 +3,38 @@ package ru.tinkoff.edu.java.bot.bot;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.abilitybots.api.bot.AbilityBot;
+import org.telegram.abilitybots.api.bot.BaseAbilityBot;
 import org.telegram.abilitybots.api.objects.Ability;
+import org.telegram.abilitybots.api.objects.Flag;
+import org.telegram.abilitybots.api.objects.Reply;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import static org.telegram.abilitybots.api.util.AbilityUtils.getChatId;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 import static org.telegram.abilitybots.api.objects.Locality.ALL;
 import static org.telegram.abilitybots.api.objects.Privacy.PUBLIC;
+import static org.telegram.abilitybots.api.util.AbilityUtils.getChatId;
+
 @Component
 public class TinkoffBot extends AbilityBot {
+    public Reply sayYuckOnImage() {
+        // getChatId is a public utility function in rg.telegram.abilitybots.api.util.AbilityUtils
+        BiConsumer<BaseAbilityBot,Update> action = (bot,upd) -> silent.send("Wrong", getChatId(upd));
 
+
+        return Reply.of(action, hasText());
+    }
+
+    public Predicate<Update> hasText(){
+        return update -> !(update.getMessage().getText().toLowerCase().contains("/start")
+        ||update.getMessage().getText().toLowerCase().contains("/help")
+                ||update.getMessage().getText().toLowerCase().contains("/track")
+                ||update.getMessage().getText().toLowerCase().contains("/untrack")
+                ||update.getMessage().getText().toLowerCase().contains("/list"))
+                &&update.getMessage().getText().toLowerCase().contains("/");
+    }
     public Ability startCommand() {
         return Ability
                 .builder()
@@ -55,8 +80,6 @@ public class TinkoffBot extends AbilityBot {
                 .action(ctx -> silent.send("The command 'list' was used. That's real sh*t", ctx.chatId()))
                 .build();
     }
-
-
 
 
     @Override
